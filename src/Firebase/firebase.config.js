b50d2c8c5fde8828903db0a3de6a5b5ca9a748b0
Hera <mostafizhera@gmail.com>
2024-12-08 17:13:23 +0600

    added the env confinger

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,7 +2,15 @@
 import { initializeApp } from "firebase/app";
 
 // Firebase configuration
-
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_measurementId,
+  measurementId: import.meta.env.VITE_appId,
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
